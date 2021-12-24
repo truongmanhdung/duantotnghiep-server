ac63ef6affad513dfce6d0571f488b6f92c7064c
truongmanhdung <dungtmph12934@fpt.edu.vn>
2021-12-24 22:26:01 +0700

    update database

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import { createOrUpdateUser, currentUser } from '../controllers/auth';
-import { adminCheck, checkAuth } from '../middlewares/auth';
 const router = express.Router();
 
-router.post('/create-or-update-user', checkAuth, createOrUpdateUser);
-router.post('/current-user', checkAuth, currentUser);
-router.post('/current-admin', checkAuth, adminCheck, currentUser);
+router.post('/create-or-update-user', () => {
+
+});
+
 module.exports = router;
